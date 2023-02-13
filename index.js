81959c23ee0cf7d65a642b8e5547853054bbfb0e
capricia <capriciaw16@gmail.com>
2023-02-13 13:10:43 -0600

    updates

---
+++ index.js
@@ -0,0 +1,137 @@
+const fs = require('fs')
+const inquirer = require('inquirer')
+const Manager = require('./lib/Manager')
+const Engineer = require('./lib/Engineer')
+const Intern = require('./lib/Intern')
+const generateHTML = require('./src/generateHTML')
+
+let employees = [];
+//function startPrompt()
+ {
+    inquirer
+    .prompt([
+        {
+          type: "text",
+          message: "Enter team manager's name.",
+          name: "name",
+        },
+        {
+          type: "text",
+          message: "Enter team manager's ID number.",
+          name: "id",
+        },
+        {
+          type: "input",
+          message: "Enter team manager's email address.",
+          name: "email",
+        },
+        {
+          type: "text",
+          message: "Enter team manager's office number.",
+          name: "office",
+        },
+      ])
+      
+      .then(({name, id, email, office}) => {
+        let employee = new Manager(name, id, email, office);
+        employeeArr.push(employee);
+        secondPrompt();
+      });
+    }
+    
+    // starts second prompt
+    function secondPrompt() {
+      inquirer
+        .prompt({
+          type: "list",
+          message: "Add more team members",
+          choices: ["Engineer", "Intern", "Finish"],
+          name: "emp",
+        })
+    
+        .then(({ emp }) => {
+          if (emp === "Engineer") {
+            engineerPrompt();
+          } else if (emp === "Intern") {
+            internPrompt();
+          } else {
+            writePage();
+          }
+        });
+    }
+    
+    // prompts for intern info
+    function internPrompt() {
+      inquirer
+      .prompt([
+        {
+          type: "text",
+          message: "Enter intern's name.",
+          name: "name",
+        },
+        {
+          type: "text",
+          message: "Enter intern's ID number.",
+          name: "id",
+        },
+        {
+          type: "text",
+          message: "Enter intern's email.",
+          name: "email",
+        },
+        {
+          type: "text",
+          message: "Enter intern's school.",
+          name: "school",
+        },
+      ])
+  
+        .then(({name, id, email, school}) => {
+          let employee = new Intern(name, id, email, school);
+          employeeArr.push(employee);
+          secondPrompt();
+        });
+      }
+  
+  // prompts for engineer info
+  function engineerPrompt() {
+    inquirer
+      .prompt([
+        {
+          type: "text",
+          message: "Enter engineer's name.",
+          name: "name",
+        },
+        {
+          type: "text",
+          message: "Enter engineer's ID number.",
+          name: "id",
+        },
+        {
+          type: "text",
+          message: "Enter engineer's email",
+          name: "email",
+        },
+        {
+          type: "text",
+          message: "Enter engineer's Github username.",
+          name: "github",
+        },
+      ])
+  
+        .then(({name, id, email, github}) => {
+          let employee = new Engineer(name, id, email, github);
+          employeeArr.push(employee);
+          secondPrompt();
+        });
+  }
+  
+  // writes generated html page
+  function writePage() {
+    const pageHTML = generateHTML(employeeArr);
+    fs.writeFile("./dist/index.html", pageHTML, (err) => {
+      if (err) throw err;
+    });
+  }
+  
+  startPrompt();
---
+++ scr/generateHTML.js
@@ -0,0 +1,52 @@
+function renderCards(employeeArr) {
+    console.log(employeeArr);
+    return employeeArr
+      .map((employee) => {
+        let additionalInfo = "";
+        if(employee.getRole() === "Manager") {
+          additionalInfo = `Office number: ` + employee.office
+        } else if (employee.getRole() === "Engineer") {
+          additionalInfo = `Github: <a href="https://github.com/` + employee.github + `/">` + employee.github + `</a>`
+        } else if (employee.getRole() === "Intern") {
+          additionalInfo = `School: ` + employee.school
+        }
+        console.log(employee);
+        return `<article class="card col-3 m-2">
+          <div class="card-header background">
+              <h2>${employee.getName()}</h2>
+              <h4>${employee.getRole()}</h4>
+          </div>
+          <div class="card-body">
+              <p>Employee ID: ${employee.id}</p>
+              <p>Email: <a href="mailto:${employee.email}">${employee.email}</a></p>
+             <p>${additionalInfo}</p>
+          </div>
+          </article>`;
+      })
+      .join("");
+  }
+
+  const generateHTML = (employeeArr) => {
+    return `
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+          <meta charset="UTF-8">
+          <meta http-equiv="X-UA-Compatible" content="IE=edge">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Team-generator</title>
+          <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+          <link rel="stylesheet" href="./style.css" />
+      </head>
+      <body>
+          <header>
+              <h1>${employeeArr[0].getName()}'s Web Development Team</h1>
+          </header>
+          <main class="row justify-content-center">
+              ${renderCards(employeeArr)}
+          </main>
+      </body>
+      </html>
+      `;
+  };
+  module.exports = generateHTML;
